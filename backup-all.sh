2da4976f69be62db123ad000d565b4e042aa7fd4
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2013-06-23 13:44:52 -0500

    correct directories in source spec

--- backup-all.sh
+++ backup-all.sh
@@ -13,50 +13,61 @@
 
 set -u # exit over uninitialised variables
 
-T=""; # dry run, test mode OFF
-T="-n"; # dry run, test mode ON
-opts="--exclude='*/.git'"
-opts=""
-opts="$opts $T "
-# use default identy, ssh key, don't need to pass parms to SSH
-# -i, --itemize-changes       output a change-summary for all updates
-#     --log-file=FILE         log what we're doing to the specified FILE
-#     -v, --verbose               increase verbosity
-#     -a, --archive               archive mode; same as -rlptgoD (no -H)
-#     -r, --recursive             recurse into directories
-#     -t, --times                 preserve times
-
 ## LACIE drive
-# logging and log naming.
-LOGNUM=`date +%s`
-LOGNAME='lacie'
-LOGDIR="./logs"
-LOGEXT="log"
-LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
+function RunLacie {
+  T="-n"; # dry run, test mode ON
+  T=""; # dry run, test mode OFF
+  opts="--exclude='*/.git'"
+  opts="--exclude='VIDEOS/_MISC/'"
+  opts="$opts $T "
+  # use default identy, ssh key, don't need to pass parms to SSH
+  # -i, --itemize-changes       output a change-summary for all updates
+  #     --log-file=FILE         log what we're doing to the specified FILE
+  #     -v, --verbose               increase verbosity
+  #     -a, --archive               archive mode; same as -rlptgoD (no -H)
+  #     -r, --recursive             recurse into directories
+  #     -t, --times                 preserve times
 
-rsync $opts -i --log-file=$LOGFILE --delete -avv       \
-~/Excludes/BACKUP/2012\ Feast\ -\ Kauai\ Photographs   \ 
-~/Excludes/SD\ cards                                   \
-~/Excludes/Star_Trek                                   \
-/Volumes/LacieRugged/BACKUP-RSYNC
+  # logging and log naming.
+  LOGNUM=`date +%s`
+  LOGNAME='lacie'
+  LOGDIR="./logs"
+  LOGEXT="log"
+  LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
 
-
+#  rsync $opts -i --log-file=$LOGFILE --delete -avv       \
+  rsync $opts --log-file=$LOGFILE --delete -avv       \
+  ~/Excludes/BACKUP/2012\ Feast\ -\ Kauai\ Photographs   \ 
+#  ~/Excludes/SD\ cards                                   \
+  ~/Excludes/VIDEOS/Star_Trek                            \
+  /Volumes/LacieRugged/BACKUP-RSYNC
+}
+# RunLacie
 
 
 ## MAXTOR drive
-# logging and log naming.
-LOGNUM=`date +%s`
-LOGNAME='maxtor'
-LOGDIR="./logs"
-LOGEXT="log"
-LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
+function RunMaxtor {
+  T="-n"; # dry run, test mode ON
+  T=""; # dry run, test mode OFF
+  T="-n"; # dry run, test mode ON
+  opts=""
+  opts="--exclude='VIDEOS/_MISC/'"
+  opts="$opts $T "
 
-rsync $opts -i --log-file=$LOGFILE --delete -avv    \
-~/Excludes/SD\ cards                                \
-~/Excludes/Star_Trek                                \
-~/Excludes/Star_Wars                                \
-~/Excludes/cloud.Lyndell.NET                        \
-~/Excludes/VIDEOS                                   \
-~/Excludes/2012\ Chili\ Supper\ -\ Source           \
-/Volumes/Maxtor/BACKUP-RSYNC
+  # logging and log naming.
+  LOGNUM=`date +%s`
+  LOGNAME='maxtor'
+  LOGDIR="./logs"
+  LOGEXT="log"
+  LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
 
+  rsync $opts -i --log-file=$LOGFILE --delete -avv    \
+  ~/Excludes/SD\ cards                                \
+  ~/Excludes/VIDEOS/Star_Trek                         \
+  ~/Excludes/VIDEOS/Star_Wars                         \
+  ~/Excludes/cloud.Lyndell.NET                        \
+  ~/Excludes/VIDEOS                                   \
+  ~/Excludes/2012\ Chili\ Supper\ -\ Source           \
+  /Volumes/Maxtor/BACKUP-RSYNC
+}
+RunMaxtor
