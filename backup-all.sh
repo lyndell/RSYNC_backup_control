e38d4d5372bfaf8b74d0342ab1727f93411cdaaa
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2013-06-23 11:55:55 -0500

    tweak, adjust logging, commit, remove unecessary lines

--- backup-all.sh
+++ backup-all.sh
@@ -11,25 +11,12 @@
 #
 #
 
-# import settings
-# TODO; test for parameter; required
-# TODO; test for directories listed in DIR parms
-
 set -u # exit over uninitialised variables
-
-# logging and log naming.
-LOGNUM=`date +%s`
-LOGNAME='rebackup'
-LOGDIR="./logs"
-LOGEXT="log"
-LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
-
-# Sources:
-# ~/Exclude/BACKUP/2012\ Feast\ -\ Kauai\ Photographs
 
 T=""; # dry run, test mode OFF
 T="-n"; # dry run, test mode ON
-opts="--exclude='.git'"
+opts="--exclude='*/.git'"
+opts=""
 opts="$opts $T "
 # use default identy, ssh key, don't need to pass parms to SSH
 # -i, --itemize-changes       output a change-summary for all updates
@@ -40,22 +27,36 @@ opts="$opts $T "
 #     -t, --times                 preserve times
 
 ## LACIE drive
+# logging and log naming.
+LOGNUM=`date +%s`
+LOGNAME='lacie'
+LOGDIR="./logs"
+LOGEXT="log"
+LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
+
 rsync $opts -i --log-file=$LOGFILE --delete -avv       \
-~/Excludes/SD\ cards                              \
-~/Excludes/2012\ Chili\ Supper\ -\ Source       \
-~/Excludes/BACKUP/2012\ Feast\ -\ Kauai\ Photographs  \ 
-~/Excludes/Star_Trek                              \
+~/Excludes/BACKUP/2012\ Feast\ -\ Kauai\ Photographs   \ 
+~/Excludes/SD\ cards                                   \
+~/Excludes/Star_Trek                                   \
 /Volumes/LacieRugged/BACKUP-RSYNC
 
+
+
+
 ## MAXTOR drive
-rsync $opts -i --log-file=$LOGFILE --delete -avv \
-~/Excludes/SD\ cards                              \
-~/Excludes/Star_Trek                              \
-~/Excludes/Star_Wars                              \
-~/Excludes/cloud.Lyndell.NET                     \
-~/Excludes/VIDEOS                                \
+# logging and log naming.
+LOGNUM=`date +%s`
+LOGNAME='maxtor'
+LOGDIR="./logs"
+LOGEXT="log"
+LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
+
+rsync $opts -i --log-file=$LOGFILE --delete -avv    \
+~/Excludes/SD\ cards                                \
+~/Excludes/Star_Trek                                \
+~/Excludes/Star_Wars                                \
+~/Excludes/cloud.Lyndell.NET                        \
+~/Excludes/VIDEOS                                   \
+~/Excludes/2012\ Chili\ Supper\ -\ Source           \
 /Volumes/Maxtor/BACKUP-RSYNC
-
-
-tail $LOGFILE
 
