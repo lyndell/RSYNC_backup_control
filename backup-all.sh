0a6213146f46e0d2554e2def6d65cc95e57f6789
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2013-06-23 11:48:18 -0500

    all backups for testing in all scritp, remove test script

--- backup-all.sh
+++ backup-all.sh
@@ -27,6 +27,10 @@ LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
 # Sources:
 # ~/Exclude/BACKUP/2012\ Feast\ -\ Kauai\ Photographs
 
+T=""; # dry run, test mode OFF
+T="-n"; # dry run, test mode ON
+opts="--exclude='.git'"
+opts="$opts $T "
 # use default identy, ssh key, don't need to pass parms to SSH
 # -i, --itemize-changes       output a change-summary for all updates
 #     --log-file=FILE         log what we're doing to the specified FILE
@@ -36,8 +40,11 @@ LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
 #     -t, --times                 preserve times
 
 ## LACIE drive
-rsync $opts -i --log-file=$LOGFILE --delete -avv \
-~/Excludes/BACKUP/VPS                             \
+rsync $opts -i --log-file=$LOGFILE --delete -avv       \
+~/Excludes/SD\ cards                              \
+~/Excludes/2012\ Chili\ Supper\ -\ Source       \
+~/Excludes/BACKUP/2012\ Feast\ -\ Kauai\ Photographs  \ 
+~/Excludes/Star_Trek                              \
 /Volumes/LacieRugged/BACKUP-RSYNC
 
 ## MAXTOR drive
@@ -45,7 +52,7 @@ rsync $opts -i --log-file=$LOGFILE --delete -avv \
 ~/Excludes/SD\ cards                              \
 ~/Excludes/Star_Trek                              \
 ~/Excludes/Star_Wars                              \
-~/Excludes/BACKUP.VPS                            \
+~/Excludes/cloud.Lyndell.NET                     \
 ~/Excludes/VIDEOS                                \
 /Volumes/Maxtor/BACKUP-RSYNC
 
