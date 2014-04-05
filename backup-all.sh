fba494695824e66df4ac3fe6957b7eb14b5569a0
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-04-04 20:07:39 -0500

    add incremental loging, remove code for Maxtor drive

--- backup-all.sh
+++ backup-all.sh
@@ -37,36 +37,10 @@ function RunLacie {
 
 #  ~/Excludes/VIDEOS/Star_Trek                             \
 #  rsync $opts -i --log-file=$LOGFILE --delete -avv       \
-  rsync $opts --log-file=$LOGFILE --delete -avv           \
+  rsync $opts --log-file=$LOGFILE --delete -avvi           \
   ~/Excludes/BACKUP                                       \
   ~/Excludes/SD\ cards                                    \
   /Volumes/LacieRugged/BACKUP-RSYNC
 }
 RunLacie
 
-
-## MAXTOR drive
-function RunMaxtor {
-  T="-n"; # dry run, test mode ON
-  T=""; # dry run, test mode OFF
-  opts=""
-  opts="--exclude='VIDEOS/_MISC/'"
-  opts="$opts $T "
-
-  # logging and log naming.
-  LOGNUM=`date +%s`
-  LOGNAME='maxtor'
-  LOGDIR="rsync.logs"
-  LOGEXT="log"
-  LOGFILE="${LOGDIR}/${LOGNAME}.${LOGNUM}.${LOGEXT}"
-
-#  ~/Excludes/VIDEOS/Star_Trek                         \
-#  ~/Excludes/VIDEOS/Star_Wars                         \
-  rsync $opts -i --log-file=$LOGFILE --delete -avv    \
-  ~/Excludes/SD\ cards                                \
-  ~/Excludes/cloud.Lyndell.NET                        \
-  ~/Excludes/VIDEOS                                   \
-  ~/Excludes/2012\ Chili\ Supper\ -\ Source           \
-  /Volumes/Maxtor/BACKUP-RSYNC
-}
-# RunMaxtor
